c61170b8a36fc320cc8c2070c50f0889e4ae11cd
Awais Mansha <awaismansha1998@gmail.com>
2021-09-17 00:31:29 +0500

    Working on Lesson 10 in Kotlin

--- app/src/main/java/com/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.gdgfinder.R
+import com.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,9 +24,23 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+
+        val binding = HomeFragmentBinding.inflate(inflater)
+
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        return view
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+            Observer<Boolean> { shouldNavigate ->
+                if (shouldNavigate == true) {
+                    val navController = binding.root.findNavController()
+                    navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                    viewModel.onNavigatedToSearch()
+                }
+            })
+
+
+        return binding.root
     }
 }
